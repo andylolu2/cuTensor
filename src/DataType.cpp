241a877daca9b8588a931135dd98ea5322956197
andylolu2 <andylolu24@gmail.com>
2024-09-29 22:42:34 +0000

    Refactor dtypes

--- src/DataType.cpp
+++ src/DataType.cpp
@@ -1,25 +1,12 @@
 #include "DataType.hpp"
 
-// size_t dtype_size(DataType dtype) {
-//   SWITCH_DATATYPE(dtype, [&] { return sizeof(T); });
-// }
+const DataType DataType::INT32 =
+    DataType("int32", sizeof(int32_t), DataType::_INT32);
+const DataType DataType::FLOAT32 =
+    DataType("fp32", sizeof(float), DataType::_FLOAT32);
 
-// std::string dtype_name(DataType dtype) {
-//   SWITCH_DATATYPE(dtype, [&] {
-//     std::string name = "";
-//     if (dtype == DataType::INT32) {
-//       name = "int32";
-//     } else if (dtype == DataType::FLOAT32) {
-//       name = "fp32";
-//     }
-//     return name;
-//   });
-// }
-
-const DataType DataType::INT32 = DataType("int32", sizeof(int32_t));
-const DataType DataType::FLOAT32 = DataType("fp32", sizeof(float));
-
-DataType::DataType(std::string name, size_t size) : name(name), size(size) {}
+DataType::DataType(std::string name, size_t size, Enum tag)
+    : name(name), size(size), tag(tag) {}
 
 bool DataType::operator==(const DataType &dtype) const {
   return &dtype == this;
--- src/DataType.hpp
+++ src/DataType.hpp
@@ -6,14 +6,16 @@
 #ifndef SWITCH_DATATYPE
 #define SWITCH_DATATYPE(dtype, template_lambda)                                \
   {                                                                            \
-    if (dtype == DataType::INT32) {                                            \
+    switch (dtype.tag) {                                                       \
+    case DataType::_INT32: {                                                   \
       using T = int32_t;                                                       \
       return template_lambda();                                                \
-    } else if (dtype == DataType::FLOAT32) {                                   \
+    }                                                                          \
+    case DataType::_FLOAT32: {                                                 \
       using T = float;                                                         \
       return template_lambda();                                                \
     }                                                                          \
-    __builtin_unreachable();                                                   \
+    }                                                                          \
   }
 #endif
 
@@ -22,6 +24,9 @@ public:
   static const DataType INT32;
   static const DataType FLOAT32;
 
+  enum Enum { _INT32, _FLOAT32 };
+  Enum tag;
+
   const std::string name;
   const size_t size;
   bool operator==(const DataType &dtype) const;
@@ -29,6 +34,6 @@ public:
   friend std::ostream &operator<<(std::ostream &os, const DataType &dtype);
 
 private:
-  DataType(std::string name, size_t size);
+  DataType(std::string name, size_t size, Enum tag);
   DataType(DataType const &) = delete;
 };
